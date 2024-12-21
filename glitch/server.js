f80236d15f946d4d07d94899d4a3faba3bef2da8
robit <36677806+robit-man@users.noreply.github.com>
2024-12-21 12:52:21 -0800

    Update server.js

--- glitch/server.js
+++ glitch/server.js
@@ -17,9 +17,8 @@ const io = new Server(server, {
   }
 });
 
-// Initialize objects to keep track of connected players and eggs
+// Initialize players object to keep track of connected players
 const players = {};
-const eggs = {};
 
 // Function to generate a random color for new players
 function getRandomColor() {
@@ -38,8 +37,8 @@ io.on('connection', (socket) => {
     color: getRandomColor() // Assign a random color
   };
 
-  // Send 'init' event to the newly connected client with current players and eggs
-  socket.emit('init', { id: socket.id, players, eggs });
+  // Send 'init' event to the newly connected client with current players
+  socket.emit('init', { id: socket.id, players });
 
   // Notify existing clients about the new player
   socket.broadcast.emit('new_player', {
@@ -50,35 +49,18 @@ io.on('connection', (socket) => {
     color: players[socket.id].color
   });
 
-  // Handle 'create_egg' events
-  socket.on('create_egg', (data) => {
-    if (!eggs[socket.id]) {
-      // Store egg data for the user
-      eggs[socket.id] = {
-        x: data.x,
-        z: data.z,
-        rotation: data.rotation
-      };
-
-      // Broadcast the new egg to other clients
-      socket.broadcast.emit('new_egg', {
-        id: socket.id,
-        x: data.x,
-        z: data.z,
-        rotation: data.rotation
-      });
-
-      console.log(`Egg created by ${socket.id} at (${data.x}, ${data.z})`);
-    }
-  });
-
-  // Handle 'move' events for players
+  // Handle 'move' events from clients
   socket.on('move', (data) => {
     if (players[socket.id]) {
       // Update the player's position and rotation
       players[socket.id].x = data.x;
       players[socket.id].z = data.z;
       players[socket.id].rotation = data.rotation;
+
+      // Optional: Update color if provided
+      if (data.color) {
+        players[socket.id].color = data.color;
+      }
 
       // Broadcast the updated state to other clients
       socket.broadcast.emit('state_update', {
@@ -106,21 +88,19 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log(`User disconnected: ${socket.id}`);
 
-    // Remove the player and their egg from the respective objects
+    // Remove the player from the players object
     delete players[socket.id];
-    delete eggs[socket.id];
 
     // Notify remaining clients about the disconnection
     socket.broadcast.emit('player_disconnected', socket.id);
-    socket.broadcast.emit('egg_disconnected', socket.id);
   });
 });
 
-// Periodically broadcast all players' and eggs' states to all clients
+// Periodically broadcast all players' states to all clients
 const BROADCAST_INTERVAL = 1000 / 60; // 60 times per second
 
 setInterval(() => {
-  io.emit('state_update_all', { players, eggs }); // Broadcast both players and eggs
+  io.emit('state_update_all', players);
 }, BROADCAST_INTERVAL);
 
 // Start the server
